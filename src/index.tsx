0ba5e354caf91973d134a78f48b1d52934bec773
AvanesyanAshot <9teron9@mail.ru>
2022-02-02 12:24:22 +0300

    feature/add dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
+  <Provider store={}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
