c3306d604bdcd4f98cc4fe3be903ad728f8bb9b3
AvanesyanAshot <9teron9@gmail.com>
2022-02-03 22:26:59 +0300

    fix/add correct redirect

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { RouteName } from "../router";
 
 const NotFound = () => {
+  const { isAuth } = useTypedSelector((state) => state.auth);
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (isAuth) {
+      return navigate(RouteName.CALENDAR);
+    } else {
+      return navigate(RouteName.LOGIN);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return <div>Not found</div>;
 };
 
