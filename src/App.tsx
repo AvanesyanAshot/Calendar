08cdbdb39c17af49a679ade41ad8235a2b4e6bb6
AvanesyanAshot <9teron9@mail.ru>
2022-02-03 11:41:58 +0300

    feature/ check isAuth localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import Layout, { Content } from "antd/lib/layout/layout";
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/Navbar";
+import { useActions } from "./hooks/useActions";
+import { IUser } from "./models/IUser";
 
 function App() {
+  const { setUser, setIsAuth } = useActions();
+
+  useEffect(() => {
+    if (localStorage.getItem("auth")) {
+      setUser({ username: localStorage.getItem("username" || "") } as IUser);
+      setIsAuth(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Layout>
       <Navbar />
